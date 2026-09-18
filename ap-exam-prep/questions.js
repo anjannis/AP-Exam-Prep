9a00bb92455d86252b1ad12f0c730ca1799f68d3
Jannis <an.jannis@icloud.com>
2026-09-18 21:55:34 +0200

    Delete ap-exam-prep directory

--- ap-exam-prep/questions.js
+++
@@ -1,6 +0,0 @@
-// Declares the question global. Chapter files in questions/ push onto QUESTIONS.
-var QUESTIONS = [];
-
-if (typeof module !== 'undefined') {
-  module.exports = { QUESTIONS: QUESTIONS };
-}
